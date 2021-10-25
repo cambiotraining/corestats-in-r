ba7a05ef64112e0bea4281c46aa3e0a291788c79
mvanrongen <martin@vanrongen.net>
2021-10-25 20:57:40 +0100

    clean-up

--- README.md
+++
@@ -1,4 +0,0 @@
-Project-Light
-=============
-
-Responsive web templates developed by Head London for the University of Cambridge
--- setup.R
+++
@@ -1,6 +0,0 @@
-# essential libraries
-library(knitr)
-suppressPackageStartupMessages(library(kableExtra))
-suppressPackageStartupMessages(library(car))
-suppressPackageStartupMessages(library(dunn.test))
-suppressPackageStartupMessages(library(tidyverse))
